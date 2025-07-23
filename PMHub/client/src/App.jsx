a5119b53c5539368de3b7dd47c741ed2860caab0
joaoevaristo11 <A51730@alunos.isel.pt>
2025-07-23 11:32:22 +0100

    Fuckig fucking

--- PMHub/client/src/App.jsx
+++ PMHub/client/src/App.jsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import Profile from './pages/Profile';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import MediaLibrary from './components/Library/MediaLibrary';
-import SearchBar from './components/Search/SearchBar';
-import MediaList from './components/Lists/MediaList';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <div className="app">
-        <SearchBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/library" component={MediaLibrary} />
-          <Route path="/lists" component={MediaList} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <h1>MyMediaShelf está a funcionar! 🎬</h1>
+      <p>Frontend React configurado com sucesso!</p>
+    </div>
   );
-};
+}
 
 export default App;
