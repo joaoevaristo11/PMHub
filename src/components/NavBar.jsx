f32413ac1522a69d13fddbc7fded8d33b5b29c60
joaoevaristo11 <A51730@alunos.isel.pt>
2025-10-05 13:35:36 +0100

    NavBar changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,30 @@
-function NavBar(){
-    return(
-        <nav className="NavBar">
-            <div className="NavBar-container">
-                <ul className = "NavBar-menu left">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#categories">Categories</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
+import { FaSearch } from "react-icons/fa";
 
-                <a href="#" onClick={()=>window.location.reload()} className="logo-center">
-                    <img src="/images/logo.png" className="logo" alt="PMHub"/>
-                </a>
+function NavBar() {
+  return (
+    <nav className="NavBar">
+      <div className="NavBar-container">
+        
+        {/* Menu */}
+        <ul className="NavBar-menu">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#categories">Categories</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#testimonals">Testimonals</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#profile">Profile</a></li>
+        </ul>
 
-                <ul className = "NavBar-menu right">
-                    <li><a href="#testimonals">Testimonals</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#profile">Profile</a></li>
-                </ul>
-            </div>
-        </nav>
-    )
+        {/* Search bar */}
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+            <button className="search-btn" onClick={''}>
+            <FaSearch />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
